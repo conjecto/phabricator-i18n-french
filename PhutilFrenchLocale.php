427ebf207865936e9077cb3c6822862d370ae3cf
Blaise <bdecarne@gmail.com>
2016-05-24 18:19:39 +0200

    initial commit

---
+++ PhutilFrenchLocale.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * French locale.
+ */
+final class PhutilFrenchLocale extends PhutilLocale
+{
+    public function getLocaleCode()
+    {
+        return 'fr_FR';
+    }
+    public function getLocaleName()
+    {
+        return pht('Français (France)');
+    }
+    public function getFallbackLocaleCode()
+    {
+        return 'en_US';
+    }
+}
